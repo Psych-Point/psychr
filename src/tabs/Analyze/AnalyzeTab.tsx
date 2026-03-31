4ec15573b06bb65dfaad567c18dcd0a37b6d55f3
Matt19-book <Landondaniel1919@gmail.com>
2026-03-31 13:55:12 -0600

    Update

--- src/tabs/Analyze/AnalyzeTab.tsx
+++ src/tabs/Analyze/AnalyzeTab.tsx
@@ -243,7 +243,7 @@ export function AnalyzeTab() {
           </div>
         }
         rightWidth="320px"
-        right={<ScriptPanel />}
+        right={<RConsole />}
       />
 
       {/* Analysis Dialogs */}
